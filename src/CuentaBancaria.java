578f7667deebaf027f2cc56371be4684fe8a21bc
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-10 17:27:05 -0500

    subiendo clase CuentaBancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -8,5 +8,26 @@
  * @author Sebastian
  */
 public class CuentaBancaria {
+    public String NumeroCuenta;
+    public String TipoCuenta;
+    public int saldo;
+    
+    public CuentaBancaria(){
+     NumeroCuenta = "Desconocido";
+     TipoCuenta = "Desconocida";
+     saldo= 0;
+    }
+    
+    public CuentaBancaria(String NumeroCuenta, String TipoCuenta){
+        this.NumeroCuenta = NumeroCuenta;
+        this.TipoCuenta = TipoCuenta;
+    }
+    
+    public CuentaBancaria(String NumeroCuenta, String TipoCuenta, int saldo){
+        this.NumeroCuenta = NumeroCuenta;
+        this.TipoCuenta = TipoCuenta;
+        this.saldo = saldo;
+    }
+  
     
 }
