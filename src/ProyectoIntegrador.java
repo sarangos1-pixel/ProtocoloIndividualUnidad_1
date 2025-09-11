d0afe4409eb1a96ec7f9c0fd77b9f2ec97e7cad7
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-10 21:28:54 -0500

    clase CuentaBancaria proyecto integrador

--- src/ProyectoIntegrador.java
+++ src/ProyectoIntegrador.java
@@ -30,10 +30,32 @@ public class ProyectoIntegrador {
         
         System.out.println("ingrese el numero de paginas del libro");
         NumPaginas = key.nextInt(); 
-        
+        key.nextLine();
  
         Libro TuLibro = new Libro(titulo, autor, NumPaginas);
         System.out.println("detalles de tu libro: " + TuLibro);
+        
+        
+        //cuenta bancaria
+        System.out.println("****CUENTA BANCARIA****");
+        
+        System.out.println("Ingrese su nombre de usuario");
+        String usuario = key.nextLine();
+        
+        System.out.println("ingrese su numero de cuenta");
+        String NumCuenta = key.nextLine();
+        
+        System.out.println("ingrese su tipo de cuenta");
+        String TipoCuenta = key.nextLine();
+        
+        System.out.println("ingrese su saldo");
+        int saldo = key.nextInt();
+        key.nextLine();
+                 
+        CuentaBancaria CuentaUsuario = new CuentaBancaria(NumCuenta, TipoCuenta, saldo);
+        System.out.println(usuario + " estos son los detalles de su cuenta bancaria: " + CuentaUsuario);
+        
+        
         
     }
    
